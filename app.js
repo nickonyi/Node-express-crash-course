4923c725c6a7949042e27dc8e26d21a9810f4c8c
nickonyi <nickoluoch1130@gmail.com>
2025-12-17 14:14:13 +0300

    creating apis with express

--- app.js
+++ app.js
@@ -9,7 +9,21 @@ const __fileName = url.fileURLToPath(import.meta.url);
 const __dirName = path.dirname(__fileName);
 
 //setup static folder
-app.use(express.static(path.join(__dirName, "public")));
+//app.use(express.static(path.join(__dirName, "public")));
+const posts = [
+  {
+    id: 1,
+    name: "Johnte",
+  },
+  {
+    id: 2,
+    nam: "collo",
+  },
+];
+
+app.get("/api/users", (req, res) => {
+  res.json(posts);
+});
 
 app.listen(PORT, () => {
   console.log(`listening at port ${PORT}`);
